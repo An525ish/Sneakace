e612afd0d78527c5517e162401d975c07f772250
An525ish <an525ish@gmail.com>
2023-07-10 15:33:11 +0530

    change color scheme of navbar and modified banner

--- Frontend/src/components/hero-slider/HeroSliderElevenSingle.js
+++ Frontend/src/components/hero-slider/HeroSliderElevenSingle.js
@@ -3,10 +3,10 @@ import PropTypes from "prop-types";
 const HeroSliderElevenSingle = ({ data }) => {
   return (
     <div
-      className="slider-height-6 d-flex align-items-center justify-content-center bg-img"
+      className="slider-height-3 d-flex align-items-end justify-content-center bg-img"
       style={{ backgroundImage: `url(${process.env.PUBLIC_URL + data.image})` }}
     >
-      <div className="slider-content-5 slider-animated-1 text-center">
+      <div className="slider-content-3 slider-content-mrg-hm17 slider-animated-1 text-center">
         <h3 className="animated">{data.title}</h3>
         <h1 className="animated">{data.subtitle}</h1>
         <p className="animated">{data.text}</p>
