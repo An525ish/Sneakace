500f475819b3b9b6a2d3f359d9d3b14cde48f69e
huzaifa9999 <mhuzaifa.0228@gmail.com>
2023-07-11 22:01:30 +0530

    implemented MVC str. with Login,singup,Logout and product Schema

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,126 @@
+require('dotenv').config();
 const express = require('express');
-require('dotenv').config()
+const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const cookieParser = require('cookie-parser');
+const authRouter = require('./routes/AuthRoutes');
+const { User } = require('./model/User');
+const { isAuth, sanitizeUser, cookieExtractor } = require('./services/common');
+const path = require('path');
+const connectDB = require("./database/db");
 
+const opts = {};
+opts.jwtFromRequest = cookieExtractor;
+opts.secretOrKey = process.env.JWT_SECRET_KEY;
 
+connectDB();
+const server = express();
 
-const app = express();
+server.get('/', (req, res) => {
+  res.send('success');
 
-app.get('/', (req, res) => {
-  res.json('success');
 });
 
 
 
-const PORT = process.env.PORT || 4040
-app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
+//middlewares
+
+server.use(express.static(path.resolve(__dirname, 'build')));
+server.use(cookieParser());
+server.use(
+  session({
+    secret: "session-secret",
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+  })
+);
+server.use(passport.authenticate('session'));
+server.use(
+  cors({
+    exposedHeaders: ['X-Total-Count'],
+  })
+);
+server.use(express.json()); // to parse req.body
+server.use('/auth', authRouter.router);
+
+
+
+// Passport Strategies
+passport.use(
+  'local',
+  new LocalStrategy({ usernameField: 'email' }, async function (
+    email,
+    password,
+    done
+  ) {
+    // by default passport uses username
+    // console.log({ email, password });
+    try {
+      const user = await User.findOne({ email: email });
+      console.log(user);
+      if (!user) {
+        return done(null, false, { message: 'invalid credentials' }); // for safety
+      }
+      crypto.pbkdf2(
+        password,
+        user.salt,
+        310000,
+        32,
+        'sha256',
+        async function (err, hashedPassword) {
+          if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+            return done(null, false, { message: 'invalid credentials' });
+          }
+          const token = jwt.sign(
+            sanitizeUser(user),
+            process.env.JWT_SECRET_KEY
+          );
+          done(null, { id: user.id, role: user.role, token }); // this lines sends to serializer
+        }
+      );
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+
+passport.use(
+  'jwt',
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    try {
+      const user = await User.findById(jwt_payload.id);
+      if (user) {
+        return done(null, sanitizeUser(user)); // this calls serializer
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
+// this creates session variable req.user on being called from callbacks
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, { id: user.id, role: user.role });
+  });
+});
+
+// this changes session variable req.user when called from authorized request
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+
+const PORT = process.env.PORT || 8080
+server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
