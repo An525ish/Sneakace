a356c4e03667bdc38ba0d3c0b03f2e29805731f7
An525ish <an525ish@gmail.com>
2023-07-12 21:26:29 +0530

    fixed login and signup controller and did some error handling

--- Backend/routes/AuthRoutes.js
+++ Backend/routes/AuthRoutes.js
@@ -4,7 +4,8 @@ const passport = require('passport');
 
 const router = express.Router();
 //  /auth is already added in base path
-router.post('/signup', createUser)
+router
+.post('/signup', createUser)
 .post('/login', passport.authenticate('local'), loginUser)
 .post('/refresh-token', refreshToken)
 .get('/check',passport.authenticate('jwt'), checkAuth)
