f21d51977d9e7cbef3f560945f1cd5ec38c56b50
huzaifa9999 <mhuzaifa.0228@gmail.com>
2023-07-19 17:05:34 +0530

    redisgned admin panel,Cupon CRUD and Css fix

--- Frontend/src/admin/Dashboard.jsx
+++ Frontend/src/admin/Dashboard.jsx
@@ -12,6 +12,10 @@ import LineChart from "./components/LineChart";
 import BarChart from "./components/BarChart";
 import StatBox from "./components/StatBox";
 import ProgressCircle from "./components/ProgressCircle";
+import PieChart from "./components/PieChart";
+import Pie2 from "./components/Pie2";
+import { Bar } from "@nivo/bar";
+import Bar2 from "./components/Bar2";
 const Dashboard = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
@@ -19,237 +23,231 @@ const Dashboard = () => {
 
   return (
     <Container>
-    <Box m="20px">
-      {/* HEADER */}
-      <Box display="flex" justifyContent="space-between" alignItems="center" >
-        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
-      </Box>
-
-      {/* GRID & CHARTS */}
-      <Box
-        display="grid"
-        gridTemplateColumns="repeat(12, 1fr)"
-        gridAutoRows="140px"
-        gap="20px"
-        sx={{
-          "& > div": { gridColumn: isNonMediumScreens ? undefined : "span 12" },
-        }}
-      >
-        {/* ROW 1 */}
-        <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <StatBox
-            title="12,361"
-            subtitle="Emails Sent"
-            progress="0.75"
-            increase="+14%"
-            icon={
-              <EmailIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
-        </Box>
-        <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <StatBox
-            title="431,225"
-            subtitle="Sales Obtained"
-            progress="0.50"
-            increase="+21%"
-            icon={
-              <PointOfSaleIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
-        </Box>
-        <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <StatBox
-            title="32,441"
-            subtitle="New Clients"
-            progress="0.30"
-            increase="+5%"
-            icon={
-              <PersonAddIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
-        </Box>
-        <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <StatBox
-            title="1,325,134"
-            subtitle="Traffic Received"
-            progress="0.80"
-            increase="+43%"
-            icon={
-              <TrafficIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
+      <Box m="20px">
+        {/* HEADER */}
+        <Box display="flex" justifyContent="space-between" alignItems="center" >
+          <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
         </Box>
 
-        {/* ROW 2 */}
+        {/* GRID & CHARTS */}
         <Box
-          gridColumn="span 8"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
+          display="grid"
+          gridTemplateColumns="repeat(12, 1fr)"
+          gridAutoRows="140px"
+          gap="20px"
+          sx={{
+            "& > div": { gridColumn: isNonMediumScreens ? undefined : "span 12" },
+          }}
         >
+          {/* ROW 1 */}
           <Box
-            mt="25px"
-            p="0 30px"
-            display="flex "
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Box>
-              <Typography
-                variant="h5"
-                fontWeight="600"
-                color={colors.grey[100]}
-              >
-                Revenue Generated
-              </Typography>
-              <Typography
-                variant="h3"
-                fontWeight="bold"
-                color={colors.greenAccent[500]}
-              >
-                $59,342.32
-              </Typography>
-            </Box>
-            <Box>
-              <IconButton>
-                <DownloadOutlinedIcon
-                  sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
-                />
-              </IconButton>
-            </Box>
-          </Box>
-          <Box height="250px" m="-20px 0 0 0">
-            <LineChart isDashboard={true} />
-          </Box>
-        </Box>
-        <Box
-          gridColumn="span 4"
-          gridRow="span 4"
-          backgroundColor={colors.primary[400]}
-          overflow="auto"
-        >
-          <Box
+            gridColumn="span 3"
+            backgroundColor={colors.primary[400]}
             display="flex"
-            justifyContent="space-between"
             alignItems="center"
-            borderBottom={`4px solid ${colors.primary[500]}`}
-            colors={colors.grey[100]}
-            p="15px"
+            justifyContent="center"
           >
-            <Typography color={colors.grey[100]} variant="h5" fontWeight="600">
-              Recent Transactions
-            </Typography>
+            <StatBox
+              title="100,00"
+              subtitle="Total Orders"
+              // progress="0.75"
+              // increase="+14%"
+              icon={
+                <EmailIcon
+                  sx={{ color: colors.greenAccent[600], fontSize: "28px" }}
+                />
+              }
+            />
           </Box>
-          {mockTransactions.map((transaction, i) => (
+          <Box
+            gridColumn="span 3"
+            backgroundColor={colors.primary[400]}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <StatBox
+              title="431,225"
+              subtitle="Total users"
+              // progress="0.50"
+              // increase="+21%"
+              icon={
+                <PointOfSaleIcon
+                  sx={{ color: colors.greenAccent[600], fontSize: "28px" }}
+                />
+              }
+            />
+          </Box>
+          <Box
+            gridColumn="span 3"
+            backgroundColor={colors.primary[400]}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <StatBox
+              title="Rs 32,441"
+              subtitle="Total Revenue"
+              // progress="0.30"
+              // increase="+5%"
+              icon={
+                <PersonAddIcon
+                  sx={{ color: colors.greenAccent[600], fontSize: "28px" }}
+                />
+              }
+            />
+          </Box>
+          <Box
+            gridColumn="span 3"
+            backgroundColor={colors.primary[400]}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <StatBox
+              title="130 Items"
+              subtitle="Out Of stock "
+              // progress="0.80"
+              // increase="+43%"
+              icon={
+                <TrafficIcon
+                  sx={{ color: colors.greenAccent[600], fontSize: "28px" }}
+                />
+              }
+            />
+          </Box>
+
+          {/* ROW 2 */}
+          <Box
+            gridColumn="span 8"
+            gridRow="span 2"
+            backgroundColor={colors.primary[400]}
+          >
             <Box
-              key={`${transaction.txId}-${i}`}
+              mt="25px"
+              p="0 30px"
+              display="flex "
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Box>
+                <Typography
+                  variant="h5"
+                  fontWeight="600"
+                  color={colors.grey[100]}
+                >
+                  Revenue Generated Per Month
+                </Typography>
+
+              </Box>
+              {/* <Box>
+                <IconButton>
+                  <DownloadOutlinedIcon
+                    sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
+                  />
+                </IconButton>
+              </Box> */}
+            </Box>
+            <Box height="250px" m="-15px 0 0 0">
+              <BarChart isDashboard={true} />
+            </Box>
+          </Box>
+          <Box
+            gridColumn="span 4"
+            gridRow="span 2"
+            backgroundColor={colors.primary[400]}
+            overflow="auto"
+          >
+            <Box
               display="flex"
               justifyContent="space-between"
               alignItems="center"
               borderBottom={`4px solid ${colors.primary[500]}`}
+              colors={colors.grey[100]}
               p="15px"
             >
-              <Box>
-                <Typography
-                  color={colors.greenAccent[500]}
-                  variant="h5"
-                  fontWeight="600"
-                >
-                  {transaction.txId}
-                </Typography>
-                <Typography color={colors.grey[100]}>
-                  {transaction.user}
-                </Typography>
-              </Box>
-              <Box color={colors.grey[100]}>{transaction.date}</Box>
-              <Box
-                backgroundColor={colors.greenAccent[500]}
-                p="5px 10px"
-                borderRadius="4px"
-              >
-                ${transaction.cost}
-              </Box>
+              <Typography color={colors.grey[100]} variant="h5" fontWeight="600">
+                Recent Transactions
+              </Typography>
             </Box>
-          ))}
-        </Box>
+            {mockTransactions.map((transaction, i) => (
+              <Box
+                key={`${transaction.txId}-${i}`}
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                borderBottom={`4px solid ${colors.primary[500]}`}
+                p="15px"
+              >
+                <Box>
+                  <Typography
+                    color={colors.greenAccent[500]}
+                    variant="h5"
+                    fontWeight="600"
+                  >
+                    {transaction.txId}
+                  </Typography>
+                  <Typography color={colors.grey[100]}>
+                    {transaction.user}
+                  </Typography>
+                </Box>
+                <Box color={colors.grey[100]}>{transaction.date}</Box>
+                <Box
+                  backgroundColor={colors.greenAccent[500]}
+                  p="5px 10px"
+                  borderRadius="4px"
+                >
+                  ${transaction.cost}
+                </Box>
+              </Box>
+            ))}
+          </Box>
 
-        {/* ROW 3 */}
-        <Box
-          gridColumn="span 4"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-          p="30px"
-        >
-          <Typography variant="h5" fontWeight="600">
-            Campaign
-          </Typography>
+          {/* ROW 3 */}
           <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            mt="25px"
+            gridColumn="span 6"
+            gridRow="span 2"
+            backgroundColor={colors.primary[400]}
+            p="15px"
+
+          // m="10px"
           >
-            <ProgressCircle size="125" />
+            <Typography variant="h5" fontWeight="600">
+              Product Stock
+            </Typography>
+            <PieChart />
+
+
+          </Box>
+          <Box
+            gridColumn="span 6"
+            gridRow="span 2"
+            backgroundColor={colors.primary[400]}
+            p="15px"
+          >
+            <Typography variant="h5" fontWeight="600">
+              Country Distribution
+            </Typography>
+            <Pie2 />
+          </Box>
+          <Box
+            gridColumn="span 12"
+            gridRow="span 2"
+            backgroundColor={colors.primary[400]}
+          >
             <Typography
               variant="h5"
-              color={colors.greenAccent[500]}
-              sx={{ mt: "15px" }}
+              fontWeight="600"
+              sx={{ padding: "30px 30px 0 30px" }}
             >
-              $48,352 revenue generated
+              Category Revenue
             </Typography>
-            <Typography>Includes extra misc expenditures and costs</Typography>
-          </Box>
-        </Box>
-        <Box
-          gridColumn="span 4"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-        >
-          <Typography
-            variant="h5"
-            fontWeight="600"
-            sx={{ padding: "30px 30px 0 30px" }}
-          >
-            Sales Quantity
-          </Typography>
-          <Box height="250px" mt="-20px">
-            <BarChart isDashboard={true} />
+            <Box height="250px" mt="-20px">
+              <Bar2 isDashboard={true} />
+            </Box>
           </Box>
         </Box>
       </Box>
-    </Box>
     </Container>
   );
 };
--- Frontend/src/admin/data/mockData.js
+++ Frontend/src/admin/data/mockData.js
@@ -330,115 +330,153 @@ export const mockTransactions = [
 
 export const mockBarData = [
   {
-    country: "AD",
-    "hot dog": 137,
-    "hot dogColor": "hsl(229, 70%, 50%)",
-    burger: 96,
-    burgerColor: "hsl(296, 70%, 50%)",
-    kebab: 72,
-    kebabColor: "hsl(97, 70%, 50%)",
-    donut: 140,
-    donutColor: "hsl(340, 70%, 50%)",
+    Month: "Jan",
+    
+    revenue: "137",
+    revenueColor: "hsl(110, 70%, 50%)",
+
+
+   
   },
-  {
-    country: "AE",
-    "hot dog": 55,
-    "hot dogColor": "hsl(307, 70%, 50%)",
-    burger: 28,
-    burgerColor: "hsl(111, 70%, 50%)",
-    kebab: 58,
-    kebabColor: "hsl(273, 70%, 50%)",
-    donut: 29,
-    donutColor: "hsl(275, 70%, 50%)",
+{
+  Month: "Feb",
+    "revenue": 55,
+      "revenueColor": "hsl(307, 70%, 50%)",
   },
-  {
-    country: "AF",
-    "hot dog": 109,
-    "hot dogColor": "hsl(72, 70%, 50%)",
-    burger: 23,
-    burgerColor: "hsl(96, 70%, 50%)",
-    kebab: 34,
-    kebabColor: "hsl(106, 70%, 50%)",
-    donut: 152,
-    donutColor: "hsl(256, 70%, 50%)",
+{
+  Month: "March",
+    "revenue": 109,
+      "revenueColor": "hsl(72, 70%, 50%)",
+
   },
-  {
-    country: "AG",
-    "hot dog": 133,
-    "hot dogColor": "hsl(257, 70%, 50%)",
-    burger: 52,
-    burgerColor: "hsl(326, 70%, 50%)",
-    kebab: 43,
-    kebabColor: "hsl(110, 70%, 50%)",
-    donut: 83,
-    donutColor: "hsl(9, 70%, 50%)",
+{
+  Month: "April",
+    "revenue": 133,
+      "revenueColor": "hsl(239, 71%, 50%)",
+
   },
-  {
-    country: "AI",
-    "hot dog": 81,
-    "hot dogColor": "hsl(190, 70%, 50%)",
-    burger: 80,
-    burgerColor: "hsl(325, 70%, 50%)",
-    kebab: 112,
-    kebabColor: "hsl(54, 70%, 50%)",
-    donut: 35,
-    donutColor: "hsl(285, 70%, 50%)",
+{
+  Month: "May",
+    revenue: 201,
+      revenueColor: "#4CCEAC"
+
+},
+{
+  Month: "June",
+    "revenue": 66,
+      "revenueColor": "hsl(208, 70%, 50%)",
+   
   },
-  {
-    country: "AL",
-    "hot dog": 66,
-    "hot dogColor": "hsl(208, 70%, 50%)",
-    burger: 111,
-    burgerColor: "hsl(334, 70%, 50%)",
-    kebab: 167,
-    kebabColor: "hsl(182, 70%, 50%)",
-    donut: 18,
-    donutColor: "hsl(76, 70%, 50%)",
+{
+  Month: "July",
+    "revenue": 80,
+      "revenueColor": "hsl(87, 70%, 50%)",
+  
   },
-  {
-    country: "AM",
-    "hot dog": 80,
-    "hot dogColor": "hsl(87, 70%, 50%)",
-    burger: 47,
-    burgerColor: "hsl(141, 70%, 50%)",
-    kebab: 158,
-    kebabColor: "hsl(224, 70%, 50%)",
-    donut: 49,
-    donutColor: "hsl(274, 70%, 50%)",
+{
+  Month: "Aug",
+    "revenue": 80,
+      "revenueColor": "hsl(87, 70%, 50%)",
+  
+  },
+{
+  Month: "Sept",
+    "revenue": 80,
+      "revenueColor": "hsl(87, 70%, 50%)",
+  
+  },
+{
+  Month: "Oct",
+    "revenue": 80,
+      "revenueColor": "hsl(87, 70%, 50%)",
+  
+  },
+{
+  Month: "Nov",
+    "revenue": 80,
+      "revenueColor": "hsl(87, 70%, 50%)",
+  
+  },
+{
+  Month: "Dec",
+    "revenue": 80,
+      "revenueColor": "hsl(87, 70%, 50%)",
+  
   },
 ];
+export const mockBarData2 = [
+  {
+    Category: "Cat1",
+    
+    revenue: "137",
+    revenueColor: "hsl(110, 70%, 50%)",
+   
+  },
+{
+  Category: "cat2",
+    "revenue": 55,
+      "revenueColor": "hsl(307, 70%, 50%)",
+  },
+{
+  Category: "cat3",
+    "revenue": 109,
+      "revenueColor": "hsl(72, 70%, 50%)",
 
+  },
+{
+  Category: "cat4",
+    "revenue": 133,
+      "revenueColor": "hsl(239, 71%, 50%)",
+
+  },
+{
+  Category: "caT5",
+    revenue: 201,
+      revenueColor: "#4CCEAC"
+
+},
+{
+  Category: "CAT6",
+    "revenue": 66,
+      "revenueColor": "hsl(208, 70%, 50%)",
+   
+  },
+{
+  Category: "cat7",
+    "revenue": 80,
+      "revenueColor": "hsl(87, 70%, 50%)",
+  
+  },
+];
 export const mockPieData = [
   {
-    id: "hack",
-    label: "hack",
+    id: "In",
+    label: "IN-stock",
     value: 239,
-    color: "hsl(104, 70%, 50%)",
+    color: "hsl(130, 70%, 50%)",
   },
   {
-    id: "make",
-    label: "make",
+    id: "Out",
+    label: "Out-of-Stock",
     value: 170,
     color: "hsl(162, 70%, 50%)",
   },
+  
+];
+export const mockPieData2 = [
   {
-    id: "go",
-    label: "go",
-    value: 322,
-    color: "hsl(291, 70%, 50%)",
+    id: "India",
+    label: "India",
+    value: 2393,
+    color: "hsl(130, 70%, 50%)",
   },
   {
-    id: "lisp",
-    label: "lisp",
-    value: 503,
-    color: "hsl(229, 70%, 50%)",
+    id: "Others",
+    label: "Others",
+    value: 2720,
+    color: "hsl(162, 70%, 50%)",
   },
-  {
-    id: "scala",
-    label: "scala",
-    value: 584,
-    color: "hsl(344, 70%, 50%)",
-  },
+  
 ];
 
 export const mockLineData = [
