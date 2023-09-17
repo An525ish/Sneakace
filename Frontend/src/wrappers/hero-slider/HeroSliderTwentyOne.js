7e95a8295abff59f5de6976a05371c5407ca147f
An525ish <an525ish@gmail.com>
2023-09-17 15:15:30 +0530

    scroll effect

--- Frontend/src/wrappers/hero-slider/HeroSliderTwentyOne.js
+++ Frontend/src/wrappers/hero-slider/HeroSliderTwentyOne.js
@@ -2,6 +2,8 @@ import { EffectFade } from 'swiper';
 import Swiper, { SwiperSlide } from "../../components/swiper";
 import sliderData from "../../data/hero-sliders/hero-slider-twenty-one.json";
 import HeroSliderTwentyOneSingle from "../../components/hero-slider/HeroSliderTwentyOneSingle.js";
+import { useEffect } from 'react';
+
 
 const params = {
   effect: "fade",
@@ -16,6 +18,18 @@ const params = {
 };
 
 const HeroSliderTwentyOne = () => {
+
+  useEffect(() => {
+    const scrollTimeout = setTimeout(() => {
+      window.scrollTo({
+        top: window.innerHeight,
+        behavior: 'smooth', 
+      });
+    }, 3000);
+    return () => clearTimeout(scrollTimeout);
+  }, []);
+
+
   return (
     <div className="slider-area">
       <div className="slider-active nav-style-1">
